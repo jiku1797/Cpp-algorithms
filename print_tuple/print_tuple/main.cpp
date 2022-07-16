f94547c55bea258522dcda8a586ee6ee554e6370
jiku1797 <jiku1797@gmail.com>
2022-07-16 10:38:31 +0200

    Another way to print a tuple

--- print_tuple/print_tuple/main.cpp
+++ print_tuple/print_tuple/main.cpp
@@ -3,6 +3,10 @@
 #include <tuple>
 #include <string>
 
+/*
+*   Two ways to print a std::tuple
+*/
+
 template <char delim, bool printIdx, typename ...Args>
 void printImpl(const Args&... args)
 {
@@ -31,9 +35,30 @@ void printTupleApplyFn(const std::tuple<Args...>& tup)
     std::apply(printImpl<delim, printIdx, Args...>, tup);
 }
 
+template <typename TupleT, std::size_t ...idxs>
+std::ostream& printImpl(std::ostream& os, const TupleT& tup, const std::index_sequence<idxs...>&)
+{
+   auto pos{ 0 };
+   auto printElem = [&pos, &os](const auto& elem)
+   {
+      if (pos > 0) os << "   ";
+      os << '[' << pos++ << ", " << elem << ']';
+   };
+
+   (..., printElem(std::get<idxs>(tup)));
+   return os;
+}
+
+template <typename ...TupleArgs>
+std::ostream& operator<< (std::ostream& os, const std::tuple<TupleArgs...> tup)
+{
+   return printImpl(os, tup, std::index_sequence_for<TupleArgs...>{});
+}
+
 int main(int argc, char* argv[])
 {
     const std::tuple myTup{3.14, std::string{"hej"}, 42.4};
-    printTupleApplyFn<' ', true>(myTup);
+    printTupleApplyFn<' ', true>(myTup); // 1st way...
+    std::cout << myTup << '\n'; // ...and 2nd way
     return 0;
 }
